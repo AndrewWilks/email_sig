9746a0da5c8bee5282f2307d99eb562d23864127
Andrew <n10768009@qut.edu.au>
2023-07-24 11:45:43 +1000

    Fixed issues with CSS and Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
-  theme: {
-    extend: {
-      screens: {},
-      visibility: ['responsive'],
-      spacing: {
-        '6': '1.5rem',
-        '3': '0.75rem',
-        '2': '0.5rem',
-      },
-      colors: {
-        gray: {
-          '700': '#4a5568',
-          '800': '#2d3748',
-        },
-      },
-      borderRadius: {
-        DEFAULT: '0.25rem',
-      },
-    },
-  },
+  content: ["./*.html"],
   plugins: [
-    function ({ addComponents }) {
-      addComponents({
+    plugin(function ({ addUtilities }) {
+      addUtilities({
         '.sr-only': {
           position: 'absolute',
           width: '1px',
@@ -38,21 +22,13 @@ module.exports = {
           marginLeft: '1.5rem',
           marginRight: '1.5rem',
         },
-        'ul li': {
+        'li': {
           marginBottom: '1.5rem',
           backgroundColor: '#4a5568',
           padding: '0.75rem 0.5rem',
           borderRadius: '0.25rem',
         },
       })
-    },
+    })
   ],
-  variants: {
-    extend: {
-      visibility: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
-    },
-  },
-  corePlugins: {
-    container: false
-  },
 }
